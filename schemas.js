833aa095c698023f4ce7217485a6d2a25a180bb7
UNKNOWN-X <71239907+UNKNOWN-X0@users.noreply.github.com>
2025-12-10 22:25:44 +0100

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -27,7 +27,7 @@ const schemas = {
     },
     minorEvents: {
         label: "Minor Events",
-        icon: `<svg class="icon" viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" stroke-width="2" fill="none">
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar mr-2 h-4 w-4" aria-hidden="true">
                   <path d="M8 2v4"></path>
                   <path d="M16 2v4"></path>
                   <rect width="18" height="18" x="3" y="4" rx="2"></rect>
